193592d84c42cf7f5f62ff588609bed5634fe5ee
Khalifa <kismaiel61@gmail.com>
2024-11-29 17:53:19 -0500

    Loops

--- app.js
+++ app.js
@@ -169,3 +169,155 @@ digit === 7? console.log('Bingo!') : console.log('Ouch !')
 let status = 'offline';
 
 let color = status === 'offline' ? 'red' : 'green';
+
+// For Loops
+// Example 1
+
+for (let i = 1; i <= 20; i+=1){
+    console.log(` The perfect square of ${i} is ${i * i}`)
+}
+
+// Example 2
+const examScores = [98,77,84,91,57,66];
+
+for (let i = 0; i < examScores.length ;i+=1){
+    console.log(` Your score is ${examScores[i]}`)
+}
+
+// Example 3
+const myStudent = [
+    {
+        firstName : 'Zeus',
+        grade : 86
+    },
+    {
+        firstName : 'Artemis',
+        grade : 97
+    },
+    {
+        firstName : 'Hera',
+        grade : 72
+    },
+    {
+        firstName : 'Apollo',
+        grade : 90
+    }
+];
+
+for (let i = 0; i < myStudent.length; i+=1){
+    console.log(`${myStudent[i].firstName} had a score of ${myStudent[i].grade}`)
+}
+
+// Finding Avergae grade of students
+let total = 0;
+for (let i =0 ; i < myStudent.length; i++){
+    total += myStudent[i].grade   
+}
+console.log(total/myStudent.length); 
+
+// Example 4
+const word = 'wabingozango';
+let reveresedWord = '';
+for (let i = word.length -1; i > -1 ;i-=1 ){
+    reveresedWord += word[i];
+    console.log(reveresedWord);
+}
+
+// Nested Loops
+// Example 1
+const gameBoard = [
+    [4,32,8,4],
+    [64,8,32,2],
+    [8,32,16,4],
+    [2,8,4,2]
+];
+
+let totalPoint = 0;
+for (let i = 0; i < gameBoard.length ; i+=1){
+    let val = gameBoard[i]
+    for (let j = 0; j < val.length; j+=1){
+        totalPoint += val[j];
+    }
+}
+console.log(totalPoint)
+
+// While Loops
+// Example 1
+const target = Math.floor(Math.random() * 10);
+let guess = Math.floor(Math.random() * 10);
+
+while(guess !==target){
+    console.log(`Target : ${target} Guess : ${target}`);
+    guess = Math.floor(Math.random() * 10);
+}
+
+// FOR...OF
+// Example 1
+
+let games = ['soccer','lotto','baseball','NFL']
+for (let item of games){
+    console.log(item);
+}
+
+// Example 2
+const magicSquare = [[2,7,6],[9,5,1],[4,3,8]];
+
+for (let i = 0; i < magicSquare.length; i++){
+    let sum = 0;
+    let row = magicSquare[i];
+    for (let j = 0; j < row.length; j++){
+        sum += row[j];   
+    }
+    console.log(`${row} sums to ${sum}`)     
+}
+
+for (num of magicSquare){
+    let bam = 0;
+    for (it of num){
+        bam += it;
+    }
+    console.log(`${num} sums to ${bam}`) 
+}
+
+// Example 3
+
+const words1 = ['mail','milk','bath','black']
+const words2 = ['box','shake','tub','berry']
+
+for ( let i = 0; i < words1.length; i++){
+    console.log(words1[i]+words2[i]);
+}
+
+// Example 4
+const movieReviews = {'killZone' : 5.5, GameBoy : 6.5, "Lord of the Rings" : 4.8, 'Merlin': 5}
+
+let totalRating = 0;
+for ( movie of Object.keys(movieReviews)){
+    console.log(movie, movieReviews[movie])
+}
+
+for (rat of Object.values(movieReviews)){
+    console.log(rat)
+    totalRating += rat
+}   
+console.log(`Total Rating is ${totalRating/Object.keys(movieReviews).length}`) 
+
+// FOR...IN
+// Example 1
+
+const jeopardyWinnings ={
+    regularPlay : 2522700,
+    watsonChallenge : 100000,
+    tournamentOfChamps : 200000,
+    battleOfHeroes : 10000
+}
+
+for ( let key in jeopardyWinnings){
+    console.log(key)
+}
+
+let totalPay = 0;
+for (let cash in jeopardyWinnings){
+    totalPay += jeopardyWinnings[cash];
+}
+console.log(`CR. 7 has a total of $${totalPay}`)
