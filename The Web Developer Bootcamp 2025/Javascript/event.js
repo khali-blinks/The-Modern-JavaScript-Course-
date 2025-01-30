22ec66ad6b7df587f0a78608594ad86aa2cbbf5a
Khalifa <kismaiel61@gmail.com>
2025-01-29 20:47:55 -0500

    RGB

---
+++ The Web Developer Bootcamp 2025/Javascript/event.js
@@ -0,0 +1,13 @@
+const btn = document.querySelector('button');
+const body = document.querySelector('body');
+const h1 = document.querySelector('h1');
+
+btn.addEventListener('click',()=>{
+    const r = Math.floor(Math.random()*255);
+    const g = Math.floor(Math.random()*255);
+    const b = Math.floor(Math.random()*255);
+    const newColor = `rgb(${r},${g},${b})`;
+    body.style.backgroundColor = newColor;
+    h1.innerText = newColor;
+
+})
